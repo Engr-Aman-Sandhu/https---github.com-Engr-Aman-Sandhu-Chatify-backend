cbbce699cf785802c9bc79a2431f8c92787f2685
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2023-05-27 04:32:06 +0500

    package json eslin and prettier script error fixed

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -1,11 +1,4 @@
-import {
-  Application,
-  json,
-  urlencoded,
-  Response,
-  Request,
-  NextFunction,
-} from 'express';
+import { Application, json, urlencoded, Response, Request, NextFunction } from 'express';
 import http from 'http';
 import cors from 'cors';
 import helmet from 'helmet';
@@ -19,10 +12,7 @@ import { createAdapter } from '@socket.io/redis-adapter';
 import 'express-async-errors';
 import { config } from './config';
 import applicationRoutes from './routes';
-import {
-  IErrorResponse,
-  CustomError,
-} from './shared/globals/helpers/error.handler';
+import { IErrorResponse, CustomError } from './shared/globals/helpers/error.handler';
 import Logger from 'bunyan';
 
 const SERVER_PORT = 5000;
@@ -48,7 +38,7 @@ export class ChatifyServer {
         name: 'session',
         keys: [config.SECRET_KEY_ONE!, config.SECRET_KEY_TWO!],
         maxAge: 24 * 7 * 3600000,
-        secure: config.NODE_ENV !== 'development',
+        secure: config.NODE_ENV !== 'development'
       })
     );
     app.use(hpp());
@@ -58,7 +48,7 @@ export class ChatifyServer {
         origin: config.CLIENT_URL,
         credentials: true,
         optionsSuccessStatus: 200,
-        methods: ['GET, POST, PUT', 'DELETE, OPTIONS'],
+        methods: ['GET, POST, PUT', 'DELETE, OPTIONS']
       })
     );
   }
@@ -75,25 +65,16 @@ export class ChatifyServer {
 
   private globalErrorHandler(app: Application): void {
     app.all('*', (req: Request, res: Response) => {
-      res
-        .status(HTTP_STATUS.NOT_FOUND)
-        .json({ message: `${req.originalUrl} not found` });
+      res.status(HTTP_STATUS.NOT_FOUND).json({ message: `${req.originalUrl} not found` });
     });
 
-    app.use(
-      (
-        error: IErrorResponse,
-        _req: Request,
-        res: Response,
-        next: NextFunction
-      ) => {
-        log.error(error);
-        if (error instanceof CustomError) {
-          return res.status(error.statusCode).json(error.serializeErrors());
-        }
-        next();
+    app.use((error: IErrorResponse, _req: Request, res: Response, next: NextFunction) => {
+      log.error(error);
+      if (error instanceof CustomError) {
+        return res.status(error.statusCode).json(error.serializeErrors());
       }
-    );
+      next();
+    });
   }
 
   private async startServer(app: Application): Promise<void> {
@@ -111,8 +92,8 @@ export class ChatifyServer {
     const io: Server = new Server(httpServer, {
       cors: {
         origin: config.CLIENT_URL,
-        methods: ['GET, POST, PUT', 'DELETE, OPTIONS'],
-      },
+        methods: ['GET, POST, PUT', 'DELETE, OPTIONS']
+      }
     });
     const pubClient = createClient({ url: config.REDIS_HOST });
     const subClient = pubClient.duplicate();
