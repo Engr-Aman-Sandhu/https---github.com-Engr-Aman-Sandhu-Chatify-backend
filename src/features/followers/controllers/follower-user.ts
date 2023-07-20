dcf52867b6d44e3af4d81dba476882b29fa14088
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2023-07-20 08:51:32 +0500

    Adding Follower to MongoDB + Worker methods

--- src/features/followers/controllers/follower-user.ts
+++ src/features/followers/controllers/follower-user.ts
@@ -7,7 +7,7 @@ import { IUserDocument } from '@user/interfaces/user.interface';
 import { IFollowerData } from '@follower/interfaces/follower.interface';
 import mongoose from 'mongoose';
 import { socketIOFollowerObject } from '@socket/follower';
-// import { followerQueue } from '@service/queues/follower.queue';
+import { followerQueue } from '@service/queues/follower.queue';
 
 const followerCache: FollowerCache = new FollowerCache();
 const userCache: UserCache = new UserCache();
@@ -32,12 +32,12 @@ export class Add {
     const addFolloweeToCache: Promise<void> = followerCache.saveFollowerToCache(`followers:${followerId}`, `${req.currentUser!.userId}`);
     await Promise.all([addFollowerToCache, addFolloweeToCache]);
 
-    // followerQueue.addFollowerJob('addFollowerToDB', {
-    //   keyOne: `${req.currentUser!.userId}`,
-    //   keyTwo: `${followerId}`,
-    //   username: req.currentUser!.username,
-    //   followerDocumentId: followerObjectId
-    // });
+    followerQueue.addFollowerJob('addFollowerToDB', {
+      keyOne: `${req.currentUser!.userId}`,
+      keyTwo: `${followerId}`,
+      username: req.currentUser!.username,
+      followerDocumentId: followerObjectId
+    });
     res.status(HTTP_STATUS.OK).json({ message: 'Following user now' });
   }
 
