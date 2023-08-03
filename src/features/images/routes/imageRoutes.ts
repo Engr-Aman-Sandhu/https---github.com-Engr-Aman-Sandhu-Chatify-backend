dbf78990308ccaf9a79ba8b31cbca42636873510
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2023-08-04 03:53:57 +0500

    Image get and delete controller functions

--- src/features/images/routes/imageRoutes.ts
+++ src/features/images/routes/imageRoutes.ts
@@ -1,8 +1,8 @@
 import express, { Router } from 'express';
 import { authMiddleware } from '@global/helpers/auth-middleware';
 import { Add } from '@image/controllers/add-image';
-// import { Delete } from '@image/controllers/delete-image';
-// import { Get } from '@image/controllers/get-images';
+import { Delete } from '@image/controllers/delete-image';
+import { Get } from '@image/controllers/get-images';
 
 class ImageRoutes {
   private router: Router;
@@ -12,13 +12,13 @@ class ImageRoutes {
   }
 
   public routes(): Router {
-    // this.router.get('/images/:userId', authMiddleware.checkAuthentication, Get.prototype.images);
+    this.router.get('/images/:userId', authMiddleware.checkAuthentication, Get.prototype.images);
 
     this.router.post('/images/profile', authMiddleware.checkAuthentication, Add.prototype.profileImage);
     this.router.post('/images/background', authMiddleware.checkAuthentication, Add.prototype.backgroundImage);
 
-    // this.router.delete('/images/:imageId', authMiddleware.checkAuthentication, Delete.prototype.image);
-    // this.router.delete('/images/background/:bgImageId', authMiddleware.checkAuthentication, Delete.prototype.backgroundImage);
+    this.router.delete('/images/:imageId', authMiddleware.checkAuthentication, Delete.prototype.image);
+    this.router.delete('/images/background/:bgImageId', authMiddleware.checkAuthentication, Delete.prototype.backgroundImage);
 
     return this.router;
   }
