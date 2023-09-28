a9f25d753a15cc8c0e5dcbf6964565796d5a1a6f
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2023-09-28 21:12:32 +0500

    random user controller function

--- src/features/user/controllers/get-profile.ts
+++ src/features/user/controllers/get-profile.ts
@@ -68,6 +68,18 @@ export class Get {
     res.status(HTTP_STATUS.OK).json({ message: 'Get user profile and posts', user: existingUser, posts: userPosts });
   }
 
+  public async randomUserSuggestions(req: Request, res: Response): Promise<void> {
+    let randomUsers: IUserDocument[] = [];
+    const cachedUsers: IUserDocument[] = await userCache.getRandomUsersFromCache(`${req.currentUser!.userId}`, req.currentUser!.username);
+    if (cachedUsers.length) {
+      randomUsers = [...cachedUsers];
+    } else {
+      const users: IUserDocument[] = await userService.getRandomUsers(req.currentUser!.userId);
+      randomUsers = [...users];
+    }
+    res.status(HTTP_STATUS.OK).json({ message: 'User suggestions', users: randomUsers });
+  }
+
   private async allUsers({ newSkip, limit, skip, userId }: IUserAll): Promise<IAllUsers> {
     let users = null;
     let type = '';
@@ -76,8 +88,8 @@ export class Get {
       type = 'redis';
       users = cachedUsers;
     } else {
-    type = 'mongodb';
-    users = await userService.getAllUsers(userId, skip, limit);
+      type = 'mongodb';
+      users = await userService.getAllUsers(userId, skip, limit);
     }
     const totalUsers: number = await Get.prototype.usersCount(type);
     return { users, totalUsers };
